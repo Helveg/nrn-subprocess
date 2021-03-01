8eab49c4efef58354210759aa86ac21eea08d2fa
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-01 17:02:01 +0100

    v1.2 Worker path & exception transmission (#1)
    * Added the worker path. Added exception transmission

--- nrnsub/__init__.py
+++ nrnsub/__init__.py
@@ -5,6 +5,8 @@ Facilitates the isolation of NEURON simulators by running them in subprocesses.
 import os
 import sys
 import codecs
+import inspect
+import tempfile
 import functools
 import subprocess as _sp
 import dill
@@ -14,18 +16,45 @@ _worker_script = os.path.join(os.path.dirname(__file__), "_worker.py")
 _boundary = "|!|!|!|!|!|!|!|!|!|"
 _boundary_bytes = bytes(_boundary, "utf-8")
 
-def subprocess(f, *args, **kwargs):
-    result = _invoke(f, args, kwargs)
+def _get_obj_module_path(obj):
+    p = inspect.getfile(obj)
+    d = os.path.dirname(p)
+    if p.endswith("__init__.py"):
+        return os.path.dirname(d)
+    return d
+
+def subprocess(f, *args, _worker_path=None, **kwargs):
+    if _worker_path is None:
+        _worker_path = []
+    result = _invoke(f, args, kwargs, _worker_path)
     return result
 
-def _invoke(f, args, kwargs):
+def _invoke(f, args, kwargs, paths):
     objstr = _obj2str((f, args, kwargs))
+    # Virtual files to write subprocess' stdout & stderr to.
+    o, e = tempfile.SpooledTemporaryFile(), tempfile.SpooledTemporaryFile()
     try:
-        out = _sp.check_output(["python", _worker_script, objstr])
-    except _sp.CalledProcessError as e:
-        print(e.output)
-    else:
-        return _unpack_worker_result(out)
+        try:
+            process = _sp.Popen([sys.executable, _worker_script, objstr, repr(paths)], stdout=o, stderr=e)
+            process.communicate()
+        except Exception as e:
+            print("Uncaught Popen exception.")
+            raise
+        if process.returncode:
+            # Exit code nonzero: ERR
+            # Unpack the Exception that the worker has written to stderr and
+            # raise it.
+            e.seek(0)
+            err = _unpack_worker_result(e.read())
+            raise err
+        else:
+            # Exit code 0: OK
+            # Unpack the results that the worker wrote to stdout
+            o.seek(0)
+            return _unpack_worker_result(o.read())
+    finally:
+        o.close()
+        e.close()
 
 def _obj2str(obj):
     dillbytes = dill.dumps(obj)
@@ -42,6 +71,9 @@ def _unpack_worker_data(data):
 def _write_worker_result(result):
     sys.stdout.write(_boundary + _obj2str(result) + _boundary)
 
+def _write_worker_error(err):
+    sys.stderr.write(_boundary + _obj2str(err) + _boundary)
+
 def _unpack_worker_result(result):
     c = result.count(_boundary_bytes)
     if c != 2:
@@ -50,12 +82,17 @@ def _unpack_worker_result(result):
     bytestream = codecs.decode(b64bytes, "base64")
     return dill.loads(bytestream)
 
-def isolate(f):
+def isolate(f=None, worker_path=None):
     """
     Decorator to run the decorated function in an isolated subprocess.
     """
     @functools.wraps(f)
     def subprocessor(*args, **kwargs):
-        return subprocess(f, *args, **kwargs)
-
-    return subprocessor
+        return subprocess(f, *args, _worker_path=worker_path, **kwargs)
+    if f is not None:
+        return subprocessor
+    def decorator(f_inner):
+        nonlocal f
+        f = f_inner
+        return subprocessor
+    return decorator
