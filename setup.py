9065b6876306734e4acb00cff4ec28aaf27f4424
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-23 16:46:35 +0100

    Package the package. Duh.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="nrn-subprocess",
     author="Robin De Schepper",
-    version="1.3.1",
+    version="1.3.2",
+    packages=["nrnsub"],
     install_requires=["dill", "tblib"]
 )
